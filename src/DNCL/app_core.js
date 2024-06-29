693250e5a2a976fe1a5acfa761ae45a5758b877b
inagakih120418 <inagakih120418@gmail.com>
2024-06-30 05:03:34 +0900

    update

---
+++ src/DNCL/app_core.js
@@ -0,0 +1,40 @@
+// Blocklyの初期化
+var workspace = Blockly.inject('blocklyDiv', {
+    toolbox: document.getElementById('toolbox'),
+    grid: {
+      spacing: 20,
+      length: 3,
+      colour: '#ccc',
+      snap: true
+    },
+    trashcan: true,
+    scrollbars: true,
+    zoom: {
+      controls: true,
+      wheel: true,
+      startScale: 1.0,
+      maxScale: 3,
+      minScale: 0.3,
+      scaleSpeed: 1.2
+    }
+  });
+  
+  // ユーザーが作成したコードを実行
+  function runCode() {
+    var code = Blockly.JavaScript.workspaceToCode(workspace);
+    try {
+      var outputArea = document.getElementById('output');
+      outputArea.textContent = '';
+      console['log'] = function(message) {
+        outputArea.textContent += message + '\n';
+      };
+      eval(code);
+    } catch (e) {
+      alert(e);
+    }
+  }
+  
+  // 初期ブロック配置
+  var workspaceBlocks = Blockly.Xml.textToDom('<xml xmlns="https://developers.google.com/blockly/xml"></xml>');
+  Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
+  
